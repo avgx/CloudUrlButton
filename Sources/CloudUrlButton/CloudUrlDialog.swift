e34f972a4f283d663fb6fc3978a9ad1e3be1143f
Артём Черныш <artemchernysh@192.168.1.73>
2023-12-22 18:03:53 +0300

    Add new EditButton

--- Sources/CloudUrlButton/CloudUrlDialog.swift
+++ Sources/CloudUrlButton/CloudUrlDialog.swift
@@ -1,6 +1,6 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Alexey Govorovsky on 21.12.2023.
 //
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct CloudUrlDialog: View {
-
+    
     @Environment(\.cloudUrl)
     private var url: Binding<URL>
     
@@ -16,7 +16,10 @@ struct CloudUrlDialog: View {
     private var focus: Int?
     
     @State
-    private var isEdited: Bool = false
+    private var isEditedForIndex: Int?
+    
+    @EnvironmentObject
+    var clouds: Clouds
     
     var body: some View {
         if #available(iOS 16.0, *) {
@@ -31,69 +34,108 @@ struct CloudUrlDialog: View {
             .navigationViewStyle(.stack)
         }
     }
-
-    @EnvironmentObject
-    var clouds: Clouds
-
+    
     @ViewBuilder
     var content: some View {
         List {
             ForEach(Array(clouds.cloud.enumerated()), id: \.offset) { index, cloud in
-                TextField("Cloud name", text: Binding<String> (
-                    get: {
-                        cloud
-                    },
-                    set: { newValue in
-                        clouds.cloud[index] = newValue
-                    }))
-                .focused($focus, equals: index)
-                .disabled(!isEdited)
-                .onTapGesture {
-                    clouds.actualCloudIndex = index
-                }
-                .swipeActions {
-                    Button(action: {
-                        if clouds.cloud.count > 1 &&
-                            index != clouds.cloud.count - 1 {
-                            clouds.cloud.remove(at: index)
-                        } else if clouds.cloud.count > 1 {
-                            clouds.actualCloudIndex = 0
-                            clouds.cloud.remove(at: index)
+                if isEditedForIndex == index {
+                    TextField("Cloud name", text: Binding<String> (
+                        get: {
+                            cloud
+                        },
+                        set: { newValue in
+                            clouds.cloud[index] = newValue
+                        }))
+                    .focused($focus, equals: index)
+                    .disabled(!(isEditedForIndex == index))
+                    .onTapGesture {
+                        tapCloudAction(index: index)
+                    }
+                    .swipeActions {
+                        Button(action: {
+                            deleteCloudAction(index: index)
+                        }, label: {
+                            Image(systemName: "trash")
+                        })
+                        .tint(.red)
+                        
+                        Button(action: {
+                            editCloudAction(index: index)
+                        }, label: {
+                            Image(systemName: "pencil")
+                        })
+                    }
+                } else {
+                    Text(cloud)
+                        .onTapGesture {
+                            tapCloudAction(index: index)
                         }
-                    }, label: {
-                        Image(systemName: "trash")
-                    })
-                    .tint(.red)
+                        .swipeActions {
+                            Button(action: {
+                                deleteCloudAction(index: index)
+                            }, label: {
+                                Image(systemName: "trash")
+                            })
+                            .tint(.red)
+                            
+                            Button(action: {
+                                editCloudAction(index: index)
+                            }, label: {
+                                Image(systemName: "pencil")
+                            })
+                        }
                 }
             }
+            .onDelete(perform: { _ in })
             Text("Actual cloud: \n\(clouds.cloud[clouds.actualCloudIndex])")
         }
         .navigationTitle("Select cloud")
         .toolbar(content: {
             ToolbarItem(placement: .topBarTrailing) {
-                Button(action: {
-                    isEdited.toggle()
-                }, label: {
-                    if isEdited {
-                        Text("Done")
-                            .bold()
-                    } else {
-                        Text("Edit")
-                    }
-                })
+                EditButton()
             }
             ToolbarItem(placement: .topBarLeading) {
                 Button(action: {
-                    if !isEdited {
-                        isEdited.toggle()
-                    }
-                    clouds.cloud.append("")
-                    focus = clouds.cloud.count - 1
+                    addCloudAction()
                 }, label: {
                     Text("Add")
                 })
             }
+            
         })
+    }
+    
+    private func tapCloudAction(index: Int) {
+        if isEditedForIndex == nil {
+            clouds.actualCloudIndex = index
+        } else if isEditedForIndex != index {
+            isEditedForIndex = nil
+        }
+    }
+    
+    private func deleteCloudAction(index: Int) {
+        if clouds.cloud.count > 1 &&
+            index != clouds.cloud.count - 1 {
+            clouds.cloud.remove(at: index)
+        } else if clouds.cloud.count > 1 {
+            clouds.actualCloudIndex = 0
+            clouds.cloud.remove(at: index)
+        }
+    }
+    
+    private func editCloudAction(index: Int) {
+        clouds.actualCloudIndex = index
+        isEditedForIndex = index
+        DispatchQueue.global().asyncAfter(deadline: .now() + 0.1) {
+            focus = index
+        }
+    }
+    
+    private func addCloudAction() {
+        clouds.cloud.append("")
+        isEditedForIndex = clouds.cloud.count - 1
+        focus = clouds.cloud.count - 1
     }
 }
 
